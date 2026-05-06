d47b610497a6ce7a71b580ce9205d023740eee3a
iamfazal <iamfazal@users.noreply.github.com>
2026-05-06 23:32:28 +0500

    more problems

---
+++ Leetcode/algorithms/JS/search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = ~~((left + right) / 2);
+
+        if (nums[mid] === target)
+            return mid;
+
+        if (nums[left] <= nums[mid]) {
+            // left half sorted
+            if (target >= nums[left] && target < nums[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        } else {
+            // right half sorted
+            if (nums[mid] < target && target <= nums[right])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+    }
+
+    return -1;
+};
